356ba45afec1bfa6e0c7f369dfbec28df6de3ec5
lcanet <lc@lc6.net>
2015-02-04 22:50:28 +0100

    Bacon code

---
+++ src/main/scala/codes/Bacon.scala
@@ -0,0 +1,30 @@
+package codes
+
+object Bacon {
+
+  private def padBin(to: Int)(nb: String): String =
+    if (nb.length() < to) "0" + padBin(to - 1)(nb)
+    else nb
+
+  private def pad5(nb: String) = padBin(5)(nb)
+
+  private def binVal(x: Int) = pad5(x.toBinaryString).map { v => if (v == '0') 'a' else 'b' }
+
+  private def correct(v: Char): Int =
+    if (v > 'u') v - 2
+    else if (v > 'i') v - 1
+    else v
+
+  def encode(str: String): String = str.toLowerCase().flatMap { x => binVal(correct(x) - 'a') }
+
+  private def recorrect(v: Int): Int =
+    if (v > 'u') v + 2
+    else if (v > 'i') v + 1
+    else v
+
+  private def fromBin(s: String): Int = Integer.parseInt(s.map { v => if (v == 'a') '0' else '1' }, 2)
+  private def recodeCharBloc(s: String): Int = recorrect('a' + fromBin(s))
+
+  def decode(str: String): String = str.grouped(5).map { x => recodeCharBloc(x).toChar }.mkString
+
+}
