0fdd8a279289310094df993526e63b6bd981bd5f
lcanet <lc@lc6.net>
2015-02-06 22:40:21 +0100

    Fix

--- src/main/scala/codes/Bacon.scala
+++ src/main/scala/codes/Bacon.scala
@@ -10,16 +10,16 @@ object Bacon {
 
   private def binVal(x: Int) = pad5(x.toBinaryString).map { v => if (v == '0') 'a' else 'b' }
 
-  private def correct(v: Char): Int =
+  private def correct(v: Char): Int = 
     if (v > 'u') v - 2
     else if (v > 'i') v - 1
     else v
 
   def encode(str: String): String = str.toLowerCase().flatMap { x => binVal(correct(x) - 'a') }
 
-  private def recorrect(v: Int): Int =
-    if (v > 'u') v + 2
-    else if (v > 'i') v + 1
+  private def recorrect(v: Int): Int = 
+    if (v >= 'u') v + 2
+    else if (v >= 'j') v + 1
     else v
 
   private def fromBin(s: String): Int = Integer.parseInt(s.map { v => if (v == 'a') '0' else '1' }, 2)
--- src/main/scala/codes/Saw.scala
+++ src/main/scala/codes/Saw.scala
@@ -12,13 +12,20 @@ object Saw {
     val chars1 = for (i <- 0 to mid - 1) yield str(2*i)
     val chars2 = for (i <- 0 to mid - 1) yield str(2*i+1)
     
-    appendTail(chars1.mkString + chars2.mkString, str)
+    appendTail(chars1.mkString, str) + chars2.mkString
   }                                             
 
+  private def couple(str:String, pos:Int):String = {
+    val mid = str.length() / 2
+    val offset = if ((str.length() % 2) == 1) 1 else 0
+    val last = if (mid+pos+offset < str.length()) str(mid+pos+offset) else "" 
+    str(pos) + "" + last
+  }
+  
   def decode(str:String):String = {
     val mid = str.length() / 2
     val offset = if ((str.length() % 2) == 1) 1 else 0
-    (for (i <- 0 to mid-1) yield (str(i) + "" + str(mid+i+offset))).flatten.mkString
+    (for (i <- 0 to mid) yield (couple(str,i))).flatten.mkString
   }                                             
 
 }
